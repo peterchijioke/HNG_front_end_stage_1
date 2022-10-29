a0d2507381f1ab767fa79124008a7c3edb0d77e0
Chukwu Chijioke Peter <peterchijioke1@gmail.com>
2022-10-29 18:19:33 +0100

    created a new react application and also installed node-sass

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,9 @@
+import React,{ HTMLProps, useState} from "react";
+
+const Home =(params:HTMLProps<HTMLElement>)=> {
+
+    return<div className="container">peter</div>
+    
+}
+
+export default Home
