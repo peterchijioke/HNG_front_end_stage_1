625da20418a65f043b66a367d1575049d23adc46
Chukwu Chijioke Peter <peterchijioke1@gmail.com>
2022-10-29 19:39:28 +0100

    added tmy slack user name with the p HTML tag

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,19 +1,25 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-import React,{ HTMLProps, } from "react";
+import React,{ HTMLProps, useState, } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faShare } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faEllipsis, faShare } from '@fortawesome/free-solid-svg-icons'
 import './home.scss'
 
 
 const Home =(params:HTMLProps<HTMLElement>)=> {
+
+    const [media,setMedia]=useState<boolean>( window.matchMedia("(max-width: 900px)").matches)
 let photo = '../../images/profile_img.jpg'
     return<div className="container">
             <div className="profile-section">
                <div className="avatar-share-button">
-               <FontAwesomeIcon className="icon-share" icon={faShare}/>
+               {media?<FontAwesomeIcon className="icon-share" icon={faEllipsis}/>:<FontAwesomeIcon className="icon-share" icon={faShare}/>}
                </div>
                 <img src={require(`../../images/profile_img.jpg`)} id="profile__img" alt={`profile ${photo}`}/>
-                <p className="twitter">Chukwu (Chijioke) Peter</p>
+                <p id="twitter">Chukwu (Chijioke) Peter</p>
+                <p id="slack">Chukwu (Chijioke) Peter</p>
+            </div>
+            <div>
+
             </div>
     </div>
     
