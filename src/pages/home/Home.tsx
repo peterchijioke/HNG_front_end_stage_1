c125724d0895ca5bad1b01399fc938f4f9f98a54
Chukwu Chijioke Peter <peterchijioke1@gmail.com>
2022-10-29 19:21:38 +0100

    installed fontawesom and added the shear icon to the profile section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,21 @@
-import React,{ HTMLProps, useState} from "react";
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React,{ HTMLProps, } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faShare } from '@fortawesome/free-solid-svg-icons'
+import './home.scss'
+
 
 const Home =(params:HTMLProps<HTMLElement>)=> {
-
-    return<div className="container">peter</div>
+let photo = '../../images/profile_img.jpg'
+    return<div className="container">
+            <div className="profile-section">
+               <div className="avatar-share-button">
+               <FontAwesomeIcon className="icon-share" icon={faShare}/>
+               </div>
+                <img src={require(`../../images/profile_img.jpg`)} id="profile__img" alt={`profile ${photo}`}/>
+                <p className="twitter">Chukwu (Chijioke) Peter</p>
+            </div>
+    </div>
     
 }
 
